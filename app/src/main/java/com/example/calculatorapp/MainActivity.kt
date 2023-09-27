99ae16364acbea74609bd3eb081a9833147d62b8
denysSmeiNewBom <den.smei.ds@gmail.com>
2023-09-27 10:32:33 +0300

    Added "." Changed type of operands from Int to Double Processing divide on zero

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -7,7 +7,7 @@ import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    var firstOperand: Int? = null
+    var firstOperand: Float? = null
     var tvInput: TextView? = null
     var operation: Operation? = null
     var requireVannish = true
@@ -30,7 +30,7 @@ class MainActivity : AppCompatActivity() {
     fun onOperation(view: View) {
         val sign = (view as Button).text.toString()
         operation = Operation.value(sign)
-        firstOperand = tvInput?.text.toString().toInt()
+        firstOperand = tvInput?.text.toString().toFloat()
         tvInput?.text = "0"
         requireVannish = true
     }
@@ -43,11 +43,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onCalc(view: View) {
-        val secondOperand = tvInput?.text.toString().toInt()
-        tvInput?.text = firstOperand?.let { operation?.performOperation(it,secondOperand).toString() }
-        requireVannish = true
+        val secondOperand = tvInput?.text.toString().toFloat()
 
+        val calculationResult =
+            firstOperand?.let { operation?.performOperation(it, secondOperand).toString() }
+        val formattedResult = formatResult(calculationResult)
+        tvInput?.text = formattedResult
+
+        requireVannish = true
         operation = null
         requireVannish = true
+    }
+
+    private fun formatResult(calculationResult: String?): String? {
+        if (calculationResult == "Infinity") return "Error"
+        return calculationResult?.replace(".0$".toRegex(), "") ?: calculationResult
     }
 }
--- app/src/main/java/com/example/calculatorapp/Operation.kt
+++ app/src/main/java/com/example/calculatorapp/Operation.kt
@@ -2,27 +2,27 @@ package com.example.calculatorapp
 
 enum class Operation(val sign: String) {
     PLUS("+") {
-        override fun performOperation(first: Int, second: Int): Int {
+        override fun performOperation(first: Float, second: Float): Float {
             return first + second
         }
     },
     MINUS("-") {
-        override fun performOperation(first: Int, second: Int): Int {
+        override fun performOperation(first: Float, second: Float): Float {
             return first - second
         }
     },
     DIVIDE("/") {
-        override fun performOperation(first: Int, second: Int): Int {
+        override fun performOperation(first: Float, second: Float): Float {
             return first / second
         }
     },
     MULTIPLY("*") {
-        override fun performOperation(first: Int, second: Int): Int {
+        override fun performOperation(first: Float, second: Float): Float {
             return first * second
         }
     };
 
-    abstract fun performOperation(first: Int, second: Int): Int
+    abstract fun performOperation(first: Float, second: Float): Float
 
     companion object{
         fun value(sign: String): Operation? {
